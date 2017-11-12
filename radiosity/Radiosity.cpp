8a2add6ba7512d6efdfa3de2452347068dd82a76
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-11-12 03:54:19 +0100

    Inline `cubicInterpolate`.

--- radiosity/Radiosity.cpp
+++ radiosity/Radiosity.cpp
@@ -297,15 +297,15 @@ void Calculate_Radiosity(const int iteration) {
  * First interpolate in y, followed by interpolation of results in x
  *******************************************************************/
 
-Color cubicInterpolate(Color p[4], double x) {
-  return p[1] + 0.5 * x *
-                    (p[2] - p[0] +
-                     x * (2.0 * p[0] - 5.0 * p[1] + 4.0 * p[2] - p[3] +
-                          x * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
-}
-
 Color bicubicInterpolate(Color p[4][4], double x, double y) {
   Color arr[4];
+
+  auto cubicInterpolate = [] (Color p[4], double x) -> Color {
+    return p[1] + 0.5 * x *
+                  (p[2] - p[0] +
+                    x * (2.0 * p[0] - 5.0 * p[1] + 4.0 * p[2] - p[3] +
+                        x * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
+  };
 
   arr[0] = cubicInterpolate(p[0], y);
   arr[1] = cubicInterpolate(p[1], y);
