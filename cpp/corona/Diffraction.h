f4a37775626dfa8c4abd76b80833f69caf6ea15c
Markus Reiter <me@reitermark.us>
2018-01-29 22:46:04 +0100

    Add method to calculate minimum diffraction angle.

---
+++ cpp/corona/Diffraction.h
@@ -0,0 +1,18 @@
+#ifndef __DIFFRACTION_H__
+#define __DIFFRACTION_H__
+
+#include <cmath>
+
+// https://en.wikipedia.org/wiki/Diffraction
+//   1. Single-slit diffraction
+//   2. Diffraction-limited imaging
+inline double minDiffractionAngle(double wavelength, double aperture, double focal_length) {
+  auto f_number = focal_length / aperture;
+  auto d = 1.22 * wavelength * f_number; // 2
+
+  auto sin_angle = wavelength / d; // 1
+
+  return asin(sin_angle);
+}
+
+#endif // __DIFFRACTION_H__
