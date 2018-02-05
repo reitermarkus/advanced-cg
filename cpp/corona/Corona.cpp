e9981e2482ce374f635e7c2876dec4f010068c02
Markus Reiter <me@reitermark.us>
2018-02-05 20:12:08 +0100

    Use path tracing for corona.

--- cpp/corona/Corona.cpp
+++ cpp/corona/Corona.cpp
@@ -46,13 +46,12 @@ Vector perfectReflection(Vector dir, Vector normal) {
   return dir - normal * 2 * normal.dotProduct(dir);
 }
 
-pair<long, long> traceRay(Ray &ray, HSV &emission, double aperture, double focal_length, size_t lens_id) {
+Color radiance(const Ray &ray, HSV &ray_color) {
   double t;
   size_t id = 0;
 
-  if (!intersect(ray, t, id)) { // No intersection with scene.
-    return make_pair(-1, -1);
-  }
+  if (!intersect(ray, t, id))   /* No intersection with scene */
+    return Color(0.0, 0.0, 0.0);
 
   const SceneObject* obj = objects[id];
 
@@ -64,6 +63,10 @@ pair<long, long> traceRay(Ray &ray, HSV &emission, double aperture, double focal
   if (obj->isSphere) {
     const Sphere* sphere = static_cast<const Sphere*>(obj);
     normal = (hitpoint - sphere->position).normalize();
+
+
+    // Only the light is a sphere, so return if the ray reaches the light.
+    return ray_color.toRGB();
   } else {
     const Triangle* tri = static_cast<const Triangle*>(obj);
     normal = tri->normal;
@@ -75,45 +78,33 @@ pair<long, long> traceRay(Ray &ray, HSV &emission, double aperture, double focal
   if (normal.dotProduct(ray.dir) >= 0)
     nl = -nl;
 
-  // Ray hit the lens.
-  if (id == lens_id) {
-    return make_pair(0, 0);
-  }
+  double n1 = 1.0;
+  double n2 = refractive_index_of_air(ray_color.hueAsWavelength());
 
-  auto wavelength = emission.hueAsWavelength();
-  auto min_diffraction_angle = diffractionAngle(wavelength);
+  double phi2 = acos((-ray.dir).dotProduct(-nl));
+  double phi1 = asin(n2 / n1 * sin(phi2));
 
-  Ray new_ray = Ray(hitpoint, Sampler::randomDirection(ray.dir, min_diffraction_angle));
+  auto n = nl;
+  auto q = -ray.dir;
 
-  return traceRay(new_ray, emission, aperture, focal_length, lens_id);
+  auto u = q + cos(phi2) * n / sin(phi2);
+
+  auto l = sin(phi1) * u - cos(phi1) * n;
+
+  Ray refraction_ray = Ray(hitpoint, l.normalize());
+
+  // Initially, use both reflection and trasmission.
+  return radiance(refraction_ray, ray_color);
 }
 
-int main() {
 
-  for (int i = 0; i < 10; i++) {
-    auto color = HSV::withRandomHue(1.0, 1.0);
+int main(int argc, char *argv[]) {
+  int width = 1024;
+  int height = 768;
+  int samples = (argc == 2) ? atoi(argv[1]) : 1;
 
-    auto lambda = color.hueAsWavelength();
-
-
-    auto n = refractive_index_of_air(lambda);
-
-    cout << fixed << setprecision(10)
-      << "λ = " << lambda << " nm" << endl
-      << "n = " << n << endl;
-  }
-
-  exit(0);
-
-  srand(time(0));
-
-  auto width = 1024;
-  auto height = 768;
-
-  double aperture = 2.6;
-  double focal_length = 120.0;
-
-  auto samples = 4;
+  double aperture = 0;
+  double focal_length = 96.5;
 
   for (auto &light : lights) {
     objects.push_back(&light);
@@ -122,14 +113,14 @@ int main() {
   vector<Triangle> layers;
 
 
-  for(int i = 0; i < 5; i++) {
-    const auto h = Vector( 0.0, 100.0,  0.0);
-    const auto w = Vector(100.0,  0.0,  0.0);
-    const auto d = Vector( 0.0,  0.0, 1.0);
+  for(int i = 0; i < 20; i++) {
+    const auto h = Vector( 0.0, 250.0,  0.0);
+    const auto w = Vector(250.0,  0.0,  0.0);
+    const auto d = Vector( 0.0,  0.0, 0.5);
 
-    const auto color = Color(1.0, 1.0, 1.0);
+    const auto color = Color(1.0, 1.0, 10.0);
 
-    const auto position = Vector(0.0, 0.0, 100 + i);
+    const auto position = Vector(-100, -100, 100 + i);
 
     layers.push_back(Triangle(i * d + position, w + h,     h, Color(), color, REFR)); // Front
     layers.push_back(Triangle(i * d + position,     w, w + h, Color(), color, REFR)); // Front
@@ -142,46 +133,75 @@ int main() {
     objects.push_back(&layer);
   }
 
+
   /* Set camera origin and viewing direction (negative z direction) */
-  Ray camera(Vector(50.0, 50.0, 300.0), Vector(0.0, 0.0, -1.0).normalize());
+  Ray camera(Vector(50.0, 52.0, 295.6), Vector(0.0, -0.042612, -1.0).normalize());
   Vector focal_point = camera.org + camera.dir * focal_length;
 
   /* Image edge vectors for pixel sampling */
   Vector cx = Vector(width * 0.5135 / height);
   Vector cy = (cx.crossProduct(camera.dir)).normalize() * 0.5135;
 
-  size_t lens_id = objects.size();
-  Sphere lens = Sphere(aperture, camera.org, Color(), Color(), REFR);
-  objects.push_back(&lens);
-
+  /* Final rendering */
   Image img(width, height);
 
-  long hits = 0;
+  /* Loop over image rows */
+  for (int y = 0; y < height; y++) {
+    cout << "\rRendering (" << samples * 4 << " spp) " << (100.0 * y / (height - 1)) << "%     ";
+    srand(y * y * y);
 
-  auto ray_samples = width * height * samples;
-
-  for (auto &light : lights) {
-    auto emission = HSV::rgbToHsv(Color(light.emission.x, light.emission.y, light.emission.z));
-
+    /* Loop over row pixels */
     #pragma omp parallel for schedule(dynamic, 1)
-    for (auto s = 0; s < ray_samples; s++) {
-      auto ray = Sampler::sphericalRay(light.position, light.radius);
-      auto ray_emission = HSV::withRandomHue(emission.s, emission.v);
+    for (int x = 0; x < width; x++) {
+      img.setColor(x, y, Color());
 
-      auto pixels = traceRay(ray, ray_emission, aperture, focal_length, lens_id);
+      /* 2x2 subsampling per pixel */
+      for (int sy = 0; sy < 2; sy++) {
+        for (int sx = 0; sx < 2; sx++) {
+          Color accumulated_radiance = Color();
 
-      if (!(pixels.first == -1 || pixels.second == -1)) {
-        #pragma omp critical
-        {
-          img.addColor(pixels.first, pixels.second, light.emission);
-          hits++;
+          /* Compute radiance at subpixel using multiple samples */
+          for (int s = 0; s < samples; s++) {
+            Vector dir = camera.dir;
+            Vector lens_sample_point = Vector(0.0, 0.0, 0.0);
+
+            if (aperture > 0.0) {
+              // Generate random sample on circular lens.
+              float random_radius = drand48();
+              float random_angle = drand48();
+              Vector lens_sample_point = aperture * Vector(sqrt(random_radius) * cos(2 * M_PI * random_angle), sqrt(random_radius) * sin(2 * M_PI * random_angle), 0);
+
+              dir = (dir + (focal_point - (camera.org + lens_sample_point)).normalize()).normalize();
+            }
+
+            double dx = non_uniform_filter_sample();
+            double dy = non_uniform_filter_sample();
+
+            // Ray direction into scene from camera through sample.
+            dir = cx * ((x + (sx + 0.5 + dx) / 2.0) / width - 0.5) +
+                  cy * ((y + (sy + 0.5 + dy) / 2.0) / height - 0.5) +
+                  dir;
+
+            auto start = camera.org + dir * 130;
+
+            dir = dir.normalize();
+
+            auto ray = Ray(start + lens_sample_point, dir);
+
+            auto color = HSV::withRandomHue(1.0, 1.0);
+
+            /* Accumulate radiance */
+            accumulated_radiance = accumulated_radiance + radiance(ray, color) / samples;
+          }
+
+          accumulated_radiance = accumulated_radiance.clamp(0.0, 1.0) * 0.25;
+
+          img.addColor(x, y, accumulated_radiance);
         }
       }
     }
   }
 
-  cout << hits << "/" << ray_samples << endl;
-  cout << (double)hits / (double)ray_samples * 100 << " %" << endl;
-
+  cout << endl;
   img.save("image");
 }
